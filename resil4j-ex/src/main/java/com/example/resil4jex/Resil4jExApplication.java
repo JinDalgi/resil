2eee36e5b42aff6ad9c4ac5485cefbe4aef31810
JinDalgi <dalgi5986@gmail.com>
2023-11-08 09:35:30 +0900

    create

---
+++ resil4j-ex/src/main/java/com/example/resil4jex/Resil4jExApplication.java
@@ -0,0 +1,13 @@
+package com.example.resil4jex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resil4jExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Resil4jExApplication.class, args);
+    }
+
+}
